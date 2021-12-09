f61021a1f4840541918f5c8fc7cc6d152c275184
Edris <ali_berg1@yahoo.com>
2021-12-08 20:20:15 -0800

    initial setup

---
+++ math_series/series.py
@@ -0,0 +1,77 @@
+def fibonacci(n):
+    """
+    A function to calculate the fibonacci value of a given number
+    If the number is 0 return 0
+    If the number is 1 return 1
+    If the number is greater than 1 return is found by add the two
+    previous numbers (number -1) + (number -2)
+    """
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+print("hello world")
+
+
+def lucas(n):
+    """
+    A function to calculate the lucas value of a given number
+    If the number is 0 return 2
+    If the number is 1 return 1
+    If the number is greater than 1 return is found by add the two
+    previous numbers (number -1) + (number -2)
+
+    """
+    if (n == 0):
+        return 2
+    if n == 1:
+        return 1
+    else:
+        return lucas(n-1) + lucas(n-2)
+
+
+def sum_series(num, arg2 = 0, arg3 = 1):
+    """
+    A function that takes one required arg and 2 optional args. 
+    optional args have a default values of 0 and 1.
+    If no optional values are passed the function will produce
+    fibonacci series.
+    Calling with optional values 2 and 1 will produce lucas series.
+    Other values for the optional args will produce other series.
+   
+"""
+    # The two optional parameters will have default values of 
+    # 0 and 1 and will determine the first two values for the
+    # series to be produced.
+    # [3, 3, 6, 9, 15]
+    # [3, 6, 9, 15]
+    # (4, 3, 6)
+
+    if arg2 == 0 and arg3 == 1:
+        return fibonacci(num)
+    else:
+        if arg2 == 2 and arg3 == 1:
+            return lucas(num)
+    if num == 0:
+        return arg2
+    if num == 1:
+        return arg3
+    else:
+        
+
+
+
+if __name__ == "__main__":
+    print(f'Fibonacci: {fibonacci(3)}')
+    print(f'Lucas: {lucas(15)}')
+    #generate fibonacci series
+    print(f'sum_series: {sum_series(10)}')
+    #generate lucas series
+    print(f'sum_series with agrs: {sum_series(10, 2, 1)}')
+
+    print(f'sum_series with agrs: {sum_series(10, 4, 3)}')
---
+++ tests/test_series.py
@@ -0,0 +1,77 @@
+
+from math_series.series import fibonacci, lucas, sum_series
+import pytest
+
+# fibonacci test
+def test_fib_zero():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+def test_fib_one():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
+
+def test_fib_two():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+def test_fib_three():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
+
+def test_fib_ten():
+    actual = fibonacci(10)
+    expected = 55
+    assert actual == expected
+
+def test_fib_fifteen():
+    actual = fibonacci(15)
+    expected = 600
+    assert actual != expected
+
+
+# lucas test
+def test_lucas_zero():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+def test_lucas_one():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas_two():
+    actual = lucas(2)
+    expected = 3
+    assert actual == expected
+
+def test_lucas_three():
+    actual = lucas(3)
+    expected = 4
+    assert actual == expected
+
+def test_lucas_ten():
+    actual = lucas(10)
+    expected = 123
+    assert actual == expected
+
+def test_lucas_fifteen():
+    actual = lucas(15)
+    expected = 1263
+    assert actual != expected
+
+# test sum_series
+def test_sum_series_default_args():
+    actual = sum_series(10)
+    expected = 55
+    assert actual == expected
+
+def test_sum_series_with_two_and_one():
+    actual = sum_series(10, 2, 1)
+    expected = 123
+    assert actual == expected
